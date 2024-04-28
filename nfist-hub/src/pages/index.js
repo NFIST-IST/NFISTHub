85c789ee9a3ddcbfc96f4b9f5c8a250d87edb8a5
pedrocurvo <95446270+pedrocurvo@users.noreply.github.com>
2024-04-29 00:21:31 +0100

    Changed main page

--- nfist-hub/src/pages/index.js
+++ nfist-hub/src/pages/index.js
@@ -16,8 +16,8 @@ export default function IndexPage() {
             <Seo title="Gatsby Theme Portfolio Minimal" />
             <Page useSplashScreenAnimation>
                 <HeroSection sectionId="hero" />
-                <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Blog', 'Medium']} />
-                <AboutSection sectionId="about" heading="About Portfolio Minimal" />
+                <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Blog']} />
+                <AboutSection sectionId="about" heading="NFIST (Núcleo de Física do Instituto Superior Técnico)" />
                 <InterestsSection sectionId="details" heading="Details" />
                 <ProjectsSection sectionId="features" heading="Built-in Features" />
                 <ContactSection sectionId="github" heading="Issues?" />

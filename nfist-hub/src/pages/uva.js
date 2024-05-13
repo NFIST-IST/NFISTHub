e5f4d9dffcb1228a5697ea5baa07eaf6108942b3
Pedro Curvo <pedro.mpcurvo@gmail.com>
2024-05-13 23:36:55 +0100

    [Mobilidade] - Added UvA

---
+++ nfist-hub/src/pages/uva.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NormalSection, Page, Seo } from 'dev-nfist-hub';
+
+export default function UTrechtPage() {
+    return (
+        <>
+            <Seo title="UvA" useTitleTemplate={true} noIndex={true} />
+            <Page>
+                <NormalSection sectionId="uva" heading="UvA" />
+            </Page>
+        </>
+    );
+}

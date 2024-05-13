50984d77c6758ed25c61be2bde48016f0ea97ce1
Pedro Curvo <pedro.mpcurvo@gmail.com>
2024-05-13 23:40:35 +0100

    [Mobilidade] - Added UMinho

---
+++ nfist-hub/src/pages/uminho.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NormalSection, Page, Seo } from 'dev-nfist-hub';
+
+export default function UMinhoPage() {
+    return (
+        <>
+            <Seo title="UMinho" useTitleTemplate={true} noIndex={true} />
+            <Page>
+                <NormalSection sectionId="uminho" heading="UMinho" />
+            </Page>
+        </>
+    );
+}

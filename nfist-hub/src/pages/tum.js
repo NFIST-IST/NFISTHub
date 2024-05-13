abd60e2d420a4328f0b6d9e82587aaef5a4391ac
Pedro Curvo <pedro.mpcurvo@gmail.com>
2024-05-13 23:40:09 +0100

    [Mobilidade] - Added TUM

---
+++ nfist-hub/src/pages/tum.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NormalSection, Page, Seo } from 'dev-nfist-hub';
+
+export default function TUMPage() {
+    return (
+        <>
+            <Seo title="TUM" useTitleTemplate={true} noIndex={true} />
+            <Page>
+                <NormalSection sectionId="tum" heading="TUM" />
+            </Page>
+        </>
+    );
+}

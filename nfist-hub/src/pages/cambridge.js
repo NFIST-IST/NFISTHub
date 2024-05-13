07a74d2bf467fe4b977fe4ab0f59e3b1d54fe503
Pedro Curvo <pedro.mpcurvo@gmail.com>
2024-05-13 23:35:37 +0100

    [Mobilidade] - Added Cambridge

---
+++ nfist-hub/src/pages/cambridge.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NormalSection, Page, Seo } from 'dev-nfist-hub';
+
+export default function CambridgePage() {
+    return (
+        <>
+            <Seo title="Cambridge" useTitleTemplate={true} noIndex={true} />
+            <Page>
+                <NormalSection sectionId="cambridge" heading="Cambridge" />
+            </Page>
+        </>
+    );
+}

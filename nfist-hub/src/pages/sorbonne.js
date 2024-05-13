269a9d936707f7e67bd79b527783761a062ac62a
Pedro Curvo <pedro.mpcurvo@gmail.com>
2024-05-13 23:39:50 +0100

    [Mobilidade] - Added Sorbonne

---
+++ nfist-hub/src/pages/sorbonne.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NormalSection, Page, Seo } from 'dev-nfist-hub';
+
+export default function SorbonnePage() {
+    return (
+        <>
+            <Seo title="Sorbonne" useTitleTemplate={true} noIndex={true} />
+            <Page>
+                <NormalSection sectionId="sorbonne" heading="Sorbonne" />
+            </Page>
+        </>
+    );
+}

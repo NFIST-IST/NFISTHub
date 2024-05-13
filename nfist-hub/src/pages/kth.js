a5fa0237c3a4029fede085f4fe2bb609e78fa295
Pedro Curvo <pedro.mpcurvo@gmail.com>
2024-05-13 23:37:09 +0100

    [Mobilidade] - Added KTH

---
+++ nfist-hub/src/pages/kth.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NormalSection, Page, Seo } from 'dev-nfist-hub';
+
+export default function KTHPage() {
+    return (
+        <>
+            <Seo title="KTH" useTitleTemplate={true} noIndex={true} />
+            <Page>
+                <NormalSection sectionId="kth" heading="KTH" />
+            </Page>
+        </>
+    );
+}

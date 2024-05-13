d8d14ae9087f615f1078cf1ab73eb77e6d4761d4
Pedro Curvo <pedro.mpcurvo@gmail.com>
2024-05-13 23:40:50 +0100

    [Mobilidade] - Added UTwente

---
+++ nfist-hub/src/pages/utwente.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NormalSection, Page, Seo } from 'dev-nfist-hub';
+
+export default function UTwentePage() {
+    return (
+        <>
+            <Seo title="UTwente" useTitleTemplate={true} noIndex={true} />
+            <Page>
+                <NormalSection sectionId="utwente" heading="UTwente" />
+            </Page>
+        </>
+    );
+}

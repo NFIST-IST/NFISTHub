0542331272051f34b3a3e30c6087fc5eddf02ed0
Pedro Curvo <pedro.mpcurvo@gmail.com>
2024-05-13 23:39:00 +0100

    [Mobilidade] - Added NTNU

---
+++ nfist-hub/src/pages/ntnu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NormalSection, Page, Seo } from 'dev-nfist-hub';
+
+export default function NTNUPage() {
+    return (
+        <>
+            <Seo title="NTNU" useTitleTemplate={true} noIndex={true} />
+            <Page>
+                <NormalSection sectionId="ntnu" heading="NTNU" />
+            </Page>
+        </>
+    );
+}
